6649b43311956d1fb73e158f3a3fb599bad6cdf4
murillo.gyonamine <murillo.gyonamine@MURILLO-PC>
2023-05-21 16:05:06 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca/model/BancoDeDados.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.model
+{
+    public class BancoDeDados
+    {
+        static string conexao = "server=localhost;port=3306;" +
+        "database=bd_biblioteca;uid=root;password=011221";
+        static MySqlConnection connection;
+        public MySqlConnection conectar()
+        {
+            connection = new MySqlConnection(conexao);
+            if (!connection.State.HasFlag(System.Data.ConnectionState.Open))
+            {
+                connection.Close();
+                connection.Open();
+            }
+            return connection;
+        }
+    }
+}
---
+++ Biblioteca/view/frmLogin.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Biblioteca
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lnkCadastro = new System.Windows.Forms.LinkLabel();
+            this.chkLembre = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(241)))), ((int)(((byte)(247)))));
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(40, 80);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(299, 25);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(241)))), ((int)(((byte)(247)))));
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(40, 140);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(299, 25);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(165)))), ((int)(((byte)(239)))));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLogin.Location = new System.Drawing.Point(40, 212);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(299, 41);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "ENTRAR";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblEmail.Location = new System.Drawing.Point(37, 59);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(52, 18);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblSenha.Location = new System.Drawing.Point(37, 119);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(57, 18);
+            this.lblSenha.TabIndex = 4;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTitulo.Location = new System.Drawing.Point(134, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(115, 36);
+            this.lblTitulo.TabIndex = 5;
+            this.lblTitulo.Text = "LOGIN";
+            // 
+            // lnkCadastro
+            // 
+            this.lnkCadastro.AutoSize = true;
+            this.lnkCadastro.BackColor = System.Drawing.Color.White;
+            this.lnkCadastro.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkCadastro.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lnkCadastro.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(165)))), ((int)(((byte)(239)))));
+            this.lnkCadastro.Location = new System.Drawing.Point(212, 172);
+            this.lnkCadastro.Name = "lnkCadastro";
+            this.lnkCadastro.Size = new System.Drawing.Size(127, 16);
+            this.lnkCadastro.TabIndex = 6;
+            this.lnkCadastro.TabStop = true;
+            this.lnkCadastro.Text = "Deseja se cadastar?";
+            // 
+            // chkLembre
+            // 
+            this.chkLembre.AutoSize = true;
+            this.chkLembre.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkLembre.Location = new System.Drawing.Point(40, 171);
+            this.chkLembre.Name = "chkLembre";
+            this.chkLembre.Size = new System.Drawing.Size(117, 20);
+            this.chkLembre.TabIndex = 7;
+            this.chkLembre.Text = "Lembre de mim";
+            this.chkLembre.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(386, 277);
+            this.Controls.Add(this.chkLembre);
+            this.Controls.Add(this.lnkCadastro);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.LinkLabel lnkCadastro;
+        private System.Windows.Forms.CheckBox chkLembre;
+    }
+}
